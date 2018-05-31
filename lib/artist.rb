fdfb97a43c644bdffd0747436a66dc836688b9ef
Matt Farmer <mrfarmer777@gmail.com>
2018-05-31 19:17:53 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,8 +16,7 @@ class Artist
   end
 
   def new_song(name,genre)
-    new_genre=Genre.new(genre)
-    song=Song.new(name,self,new_genre)
+        song=Song.new(name,self.name,genre)
     song
   end
 
