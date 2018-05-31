d2b5c8357fc5abe19549e5ff3922be674d57ede3
Matt Farmer <mrfarmer777@gmail.com>
2018-05-31 18:08:04 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+class Artist
+
+  attr_accessor :name, :songs, :genres
+
+  @@all=[]
+  
+  def initialize(name)
+    @name=name
+    @songs=[]
+    @@all<<self
+  end
+
+  def 
+    
+
+
+
+
+  end
+
+end
